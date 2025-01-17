f4b9f6f6a1c1c3da1c1c3589e18e5818e71d314f
Marek Kultys <marek.kultys@gmail.com>
2025-01-17 01:23:18 +0000

    Updates

--- node_modules/has/src/index.js
+++ node_modules/has/src/index.js
@@ -1,3 +1,8 @@
-var bind = require('function-bind');
+'use strict';
 
-module.exports = bind.call(Function.call, Object.prototype.hasOwnProperty);
+var hasOwnProperty = {}.hasOwnProperty;
+var call = Function.prototype.call;
+
+module.exports = call.bind ? call.bind(hasOwnProperty) : function (O, P) {
+  return call.call(hasOwnProperty, O, P);
+};
